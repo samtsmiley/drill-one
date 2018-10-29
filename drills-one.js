2b65224859ccc9ab57874b2960597b067d209c73
Sam Smiley <samtsmiley@gmail.com>
2018-10-29 11:29:38 -0700

    makes sure name is string and age is number

--- drills-one.js
+++ drills-one.js
@@ -9,17 +9,19 @@ function createGreeting(name,age) {
     if (name === undefined || age === undefined ) {
         throw new Error('Usage: createGreeting(name, age)');
     }
+    if (typeof name !== 'string' || typeof age !== 'number' ) {
+        throw new Error('Please enter a valid name and use numbers for age');
+    }
     let yob;
     try {
         yob = getYearOfBirth(age);
     } catch (e) {
         console.log(e); 
     }
-    return `Hi, my name is ${name} and I'm ${age} years old
-    I was born in ${yob}.`;
+    return `Hi, my name is ${name} and I'm ${age} years old\nI was born in ${yob}.`;
 }
 try {
-console.log(createGreeting(-1));
+console.log(createGreeting('sam',30));
 } catch (e) {
     console.log(e);
 }
