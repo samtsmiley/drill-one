214e32ebcda9b1e25235635670b5835173b0e2e2
Sam Smiley <samtsmiley@gmail.com>
2018-10-29 11:00:36 -0700

    added getyob function

--- drills-one.js
+++ drills-one.js
@@ -1,7 +1,11 @@
-function createGreeting(name,age) {
+function getYearOfBirth(age){
     const yearOfBirth = 2018 - age;
-    return `Hi, my name is ${name} and I'm ${age} years old
-    I was born in ${yearOfBirth}.`;   
+    return yearOfBirth;
 }
-
+function createGreeting(name,age) {
+    const yob = getYearOfBirth(age);
+    return `Hi, my name is ${name} and I'm ${age} years old
+    I was born in ${yob}.`; 
+      
+}
 console.log(createGreeting('sam',30));
